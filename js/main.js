7b6446783f486e01950b0d2823551227c92ec333
Ravi Rajpurohit <ravirajpurohit29@gmail.com>
2016-06-14 02:50:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,42 +4,7 @@ $(document).ready(function(){
 		zoom: 4.5,
 		maxZoom : 18,
 		minZoom : 2
-    })
-	var mapquest = new ol.layer.Tile({
-			source: new ol.source.MapQuest ({
-			layer: 'sat'
-		}),
-		visible: true,
-		name: 'mapquest'
-    });
-	var osm = new ol.layer.Tile({
-			source: new ol.source.OSM (),
-			visible: false,
-			name: 'osm'
-    });
-	var toner = new ol.layer.Tile({
-		source: new ol.source.Stamen({
-			layer: 'toner'
-		}),
-		visible: false,
-		name: 'toner'
-    });
-	var stamen = new ol.layer.Group({
-			layers: [
-				new ol.layer.Tile({
-					source: new ol.source.Stamen({
-						layer: 'watercolor'
-					})
-				}),
-				new ol.layer.Tile({
-					source: new ol.source.Stamen({
-						layer: 'terrain-labels'
-					})
-				})
-			],
-			visible: false,
-			name: 'stamen'
-    });
+    	})
 	var map = new ol.Map({
         target: 'map',
         conrols: ol.control.defaults().extend([
@@ -49,14 +14,51 @@ $(document).ready(function(){
 		renderer: 'canvas',
 		layers: [mapquest, osm, toner, stamen],
 		view: view
-    });
+    	});
 	
-	$('#layers input[type=radio]').change(function(){
-		var layer = $(this).val();
-		
-		map.getLayers().getArray().forEach(function(e){
-			var name = e.get('name');
-			e.setVisible(name == layer);
-		});
+	var geolocation = new ol.Geolocation({
+			projection: view.getProjection()
 	});
+	
+	document.getElementById('track').addEventListener('change', function() {
+		geolocation.setTracking(this.checked);
+    	});
+	
+	geolocation.on('error', function(error) {
+        var info = document.getElementById('info');
+        info.innerHTML = error.message;
+		info.style.display = '';
+    	});
+	
+	geolocation.on('change:position', function() {
+        var coordinates = geolocation.getPosition();
+        positionFeature.setGeometry(coordinates ?
+            new ol.geom.Point(coordinates) : null);
+    	});
+	
+	var accuracyFeature = new ol.Feature();
+		geolocation.on('change:accuracyGeometry', function() {
+        		accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+    		});
+	
+	var positionFeature = new ol.Feature();
+      		positionFeature.setStyle(new ol.style.Style({
+        		image: new ol.style.Circle({
+          			radius: 6,
+          			fill: new ol.style.Fill({
+            				color: '#3399CC'
+          			}),
+          			stroke: new ol.style.Stroke({
+            				color: '#fff',
+            				width: 2
+          			})
+        		})
+    		}));
+	
+	new ol.layer.Vector({
+        map: map,
+        source: new ol.source.Vector({
+          features: [accuracyFeature, positionFeature]
+        })
+    });
 });
